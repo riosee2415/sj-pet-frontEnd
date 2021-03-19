039f9ec18a8a951cbb883cdd8b884c370f1ac04b
SeoJaeWan <sjw7324@naver.com>
2021-03-19 18:46:39 +0900

    modify

--- src/Routes/Client/MM05/MM05Container.jsx
+++ src/Routes/Client/MM05/MM05Container.jsx
@@ -2,29 +2,257 @@ import React, { useEffect, useRef, useState } from "react";
 import withSplitting from "../../../Lib/withSplitting";
 const MM05Presenter = withSplitting(() => import("./MM05Presenter"));
 import { useQuery, useMutation } from "@apollo/client";
-import {} from "./MM05Queries";
+import { GET_FAQ, GET_FAQ_TOTALPAGE } from "./MM05Queries";
 import { animateScroll as scroll } from "react-scroll";
 import useInput from "../../../Components/Hooks/useInput";
 import { toast } from "react-toastify";
 
 const MM05Container = ({ history }) => {
+  ////////////// - USE STATE- ///////////////
+  const inputName = useInput("");
+  const inputMobile = useInput("");
+  const inputEmail = useInput("");
+  const inputEmailDomain = useInput("");
+  const selectEmailDomain = useInput("");
+  const inputLoc = useInput("");
+  const inputMayPay = useInput("");
+  const inputIsStore = useInput("");
+  const inputDesc = useInput("");
 
-////////////// - USE STATE- ///////////////
+  const [isAgree, setIsAgree] = useState(false);
 
-////////////// - USE QUERY- ///////////////
+  //
+  const [actionFaqView, setActionFaqView] = useState(null);
+  const [currentType, setCurrentType] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [limit, setLimit] = useState(15);
+  const [pages, setPages] = useState(null);
+  const inputSearch = useInput("");
+  const [searchValue, setSearchValue] = useState("");
 
-///////////// - USE MUTATION- /////////////
+  ////////////// - USE QUERY- ///////////////
 
-///////////// - EVENT HANDLER- ////////////
+  const { data: faqDatum, refetch: faqRefetch } = useQuery(GET_FAQ, {
+    variables: {
+      typeName: faqTypeDatum
+        ? faqTypeDatum.getFaqType[currentType].typeName
+        : "",
+      limit,
+      currentPage,
+      searchValue,
+    },
+    skip: !faqTypeDatum,
+  });
 
-////////////// - USE EFFECT- //////////////
+  const { data: pData, refetch: pRefetch } = useQuery(GET_FAQ_TOTALPAGE, {
+    variables: {
+      typeName: faqTypeDatum
+        ? faqTypeDatum.getFaqType[currentType].typeName
+        : "",
+      searchValue,
+      limit,
+    },
+    skip: !faqTypeDatum,
+  });
 
-useEffect(() => {
-  scroll.scrollTo(0);
-}, []);
+  ///////////// - USE MUTATION- /////////////
 
-return <MM05Presenter />;
+  ///////////// - EVENT HANDLER- ////////////
+  const createRequestHandler = () => {
+    if (!inputName.value || inputName.value.trim() === "") {
+      toast.error("이름을 입력해주세요.");
+      return;
+    }
+    if (!inputMobile.value || inputMobile.value.trim() === "") {
+      toast.error("연락처를 입력해주세요.");
+      return;
+    }
+    if (!inputEmail.value || inputEmail.value.trim() === "") {
+      toast.error("이메일을 입력해주세요.");
+      return;
+    }
+    if (!selectEmailDomain.value || selectEmailDomain.value.trim() === "") {
+      toast.error("이메일을 선택해주세요.");
+      return;
+    }
+    if (
+      selectEmailDomain.value === "직접 입력" &&
+      (!inputEmailDomain.value || inputEmailDomain.value.trim() === "")
+    ) {
+      toast.error("이메일을 입력해주세요.");
+      return;
+    }
+    if (!inputLoc.value || inputLoc.value.trim() === "") {
+      toast.error("창업희망지역을 입력해주세요.");
+      return;
+    }
+    if (!inputMayPay.value || inputMayPay.value.trim() === "") {
+      toast.error("예상 창업 비용을 입력해주세요.");
+      return;
+    }
+
+    if (!inputDesc.value || inputDesc.value.trim() === "") {
+      toast.error("문의 내용을 입력해주세요.");
+      return;
+    }
+    if (!isAgree.value || isAgree.value.trim() === "") {
+      toast.error("개인정보취급방침에 동의해주세요.");
+      return;
+    }
+
+    confirmAlert({
+      title: "가맹 상담",
+      message: "가맹점 상담을 작성하시겠습니까?",
+      buttons: [
+        {
+          label: "취소",
+          onClick: () => {
+            return false;
+          },
+        },
+        {
+          label: "확인",
+          onClick: () => createRequestHandlerAfter(),
+        },
+      ],
+    });
+  };
+
+  const createRequestHandlerAfter = async () => {
+    const { data } = await createDirectRequestMutation({
+      variable: {
+        name: inputName.value,
+        loc: inputLoc.value,
+        mobile: inputMobile.value,
+        mayPay: inputMayPay.value,
+        email:
+          inputEmail.value + `@` + selectEmailDomain.value === "직접 입력"
+            ? inputEmailDomain.value
+            : selectEmailDomain.value,
+        isExistStore: inputIsStore.value,
+        description: inputDesc.value,
+        isAgreement: true,
+      },
+    });
+
+    if (data.createDirectRequest) {
+      toast.error("가맹점 상담을 신청하셨습니다.");
+      inputName.setValue("");
+      inputMobile.setValue("");
+      inputEmail.setValue("");
+      inputEmailDomain.setValue("");
+      selectEmailDomain.setValue("");
+      inputLoc.setValue("");
+      inputMayPay.setValue("");
+      inputIsStore.setValue("");
+      inputDesc.setValue("");
+      setIsAgree(false);
+    } else {
+      toast.error("잠시 후 다시 시도해주세요.");
+    }
+  };
+
+  const toggleFaqAnswer = (id, content) => {
+    let currentFaqAction = actionFaqView.map((action, idx) =>
+      idx === id ? !action : action
+    );
+    const answer = document.getElementById(`faq-answer-${id}`);
+    answer.innerHTML = content;
+    setActionFaqView(currentFaqAction);
+  };
+
+  const changeFaqTypeHandler = (type) => {
+    setActionFaqView(null);
+    setCurrentType(type);
+  };
+
+  const prevAndNextPageChangeHandler = (page) => {
+    if (page < 0) {
+      toast.error("첫 페이지 입니다.");
+      return;
+    }
+
+    if (page > pages.length - 1) {
+      toast.error("마지막 페이지 입니다.");
+      return;
+    }
+
+    setCurrentPage(page);
+  };
+  const changePageHandler = (page) => {
+    setCurrentPage(page);
+  };
+
+  const searchHandler = () => {
+    setCurrentPage(0);
+    setSearchValue(inputSearch.value);
+  };
+
+  ////////////// - USE EFFECT- //////////////
+
+  useEffect(() => {
+    faqTypeRefetch();
+    faqRefetch();
+    pRefetch();
+    scroll.scrollTo(0);
+  }, []);
+
+  useEffect(() => {
+    setActionFaqView(null);
+  }, [currentPage]);
+
+  useEffect(() => {
+    if (faqDatum && !actionFaqView) {
+      let defaultAction = faqDatum.getFaqDetail.map(() => {
+        return false;
+      });
+
+      setActionFaqView(defaultAction);
+    }
+  }, [faqDatum]);
+
+  useEffect(() => {
+    if (pData) {
+      const temp = [];
+
+      for (let i = 0; i < pData.getFaqTotalPage; i++) temp.push(i);
+
+      setPages(temp);
+    }
+  }, [pData]);
+
+  return (
+    <MM05Presenter
+      //
+      inputName={inputName}
+      inputMobile={inputMobile}
+      inputEmail={inputEmail}
+      inputEmailDomain={inputEmailDomain}
+      selectEmailDomain={selectEmailDomain}
+      inputLoc={inputLoc}
+      inputMayPay={inputMayPay}
+      inputIsStore={inputIsStore}
+      inputDesc={inputDesc}
+      isAgree={isAgree}
+      setIsAgree={setIsAgree}
+      //
+      createRequestHandler={createRequestHandler}
+      ///////////////////// FAQ
+      inputSearch={inputSearch}
+      pages={pages}
+      currentPage={currentPage}
+      actionFaqView={actionFaqView}
+      currentType={currentType}
+      //
+      faqDatum={faqDatum && faqDatum.getFaqDetail}
+      //
+      toggleFaqAnswer={toggleFaqAnswer}
+      changeFaqTypeHandler={changeFaqTypeHandler}
+      prevAndNextPageChangeHandler={prevAndNextPageChangeHandler}
+      changePageHandler={changePageHandler}
+      searchHandler={searchHandler}
+    />
+  );
 };
 
 export default MM05Container;
-  
--- src/Routes/Client/MM05/MM05Queries.js
+++ src/Routes/Client/MM05/MM05Queries.js
@@ -1,2 +1,61 @@
-import { gql } from '@apollo/client';
-  
+import { gql } from "@apollo/client";
+
+export const CREATE_DIRECTREQUEST = gql`
+  mutation createDirectRequest(
+    $name: String!
+    $loc: String!
+    $mobile: String!
+    $mayPay: String!
+    $email: String!
+    $isExistStore: String!
+    $description: String!
+    $isAgreement: Boolean!
+  ) {
+    createDirectRequest(
+      name: $name
+      loc: $loc
+      mobile: $mobile
+      mayPay: $mayPay
+      email: $email
+      isExistStore: $isExistStore
+      description: $description
+      isAgreement: $isAgreement
+    )
+  }
+`;
+
+export const GET_FAQ = gql`
+  query getFaqDetail(
+    $typeName: String!
+    $searchValue: String!
+    $limit: Int!
+    $currentPage: Int!
+  ) {
+    getFaqDetail(
+      typeName: $typeName
+      searchValue: $searchValue
+      limit: $limit
+      currentPage: $currentPage
+    ) {
+      _id
+      sort
+      type
+      question
+      answer
+    }
+  }
+`;
+
+export const GET_FAQ_TOTALPAGE = gql`
+  query getFaqTotalPage(
+    $typeName: String!
+    $searchValue: String!
+    $limit: Int!
+  ) {
+    getFaqTotalPage(
+      typeName: $typeName
+      searchValue: $searchValue
+      limit: $limit
+    )
+  }
+`;
