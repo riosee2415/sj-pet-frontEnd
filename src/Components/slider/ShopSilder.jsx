4293f8a873758bc655ecc1dfa23418ea81652cac
yekyuhwang <4leaf.hyk@gmail.com>
2021-06-17 21:08:09 +0900

    shopSilder

--- src/Components/slider/ShopSilder.jsx
+++ src/Components/slider/ShopSilder.jsx
@@ -40,7 +40,7 @@ const Title = styled(Text)`
 
   @media (max-width: 800px) {
     font-size: 11px !important;
-    bottom: 10px;
+    bottom: 30px;
   }
 `;
 
@@ -119,132 +119,120 @@ export default ({ width, sDatum, isColumn, moveLinkHandler }) => {
         <SwiperSlide>
           <ShopImage>
             <Image
-              height={`240px`}
+              height={width < 1100 ? (900 ? `200px` : `120px`) : `240px`}
               alt="image"
               src={`https://firebasestorage.googleapis.com/v0/b/storage-4leaf.appspot.com/o/SJPET%2Fassets%2FImages%2F%EB%A7%A4%EC%9E%A5%2F%E1%84%83%E1%85%A9%E1%86%BC%E1%84%80%E1%85%AE%E1%84%8C%E1%85%A5%E1%86%B7.jpeg?alt=media&token=8bf45485-657d-4931-a1fd-4baf767ced29`}
             />
-
             <Title>01호_동구점</Title>
           </ShopImage>
         </SwiperSlide>
         <SwiperSlide>
           <ShopImage>
             <Image
-              height={`240px`}
+              height={width < 1100 ? (900 ? `200px` : `120px`) : `240px`}
               alt="image"
               src={`https://firebasestorage.googleapis.com/v0/b/storage-4leaf.appspot.com/o/SJPET%2Fassets%2FImages%2F%EB%A7%A4%EC%9E%A5%2F%E1%84%89%E1%85%A5%E1%84%80%E1%85%AE%E1%84%8C%E1%85%A5%E1%86%B7.jpeg?alt=media&token=96dc0b79-457b-4033-86ce-eb174798c335`}
             />
-
             <Title>02호_서구점</Title>
           </ShopImage>
         </SwiperSlide>
         <SwiperSlide>
           <ShopImage>
             <Image
-              height={`240px`}
+              height={width < 1100 ? (900 ? `200px` : `120px`) : `240px`}
               alt="image"
               src={`https://firebasestorage.googleapis.com/v0/b/storage-4leaf.appspot.com/o/SJPET%2Fassets%2FImages%2F%EB%A7%A4%EC%9E%A5%2F%E1%84%8C%E1%85%AE%E1%86%BC%E1%84%80%E1%85%AE%E1%84%8C%E1%85%A5%E1%86%B7.jpeg?alt=media&token=0c853eea-f5db-4d22-9249-e56fb31f4b68`}
             />
-
             <Title>03호_중구점</Title>
           </ShopImage>
         </SwiperSlide>
         <SwiperSlide>
           <ShopImage>
             <Image
-              height={`240px`}
+              height={width < 1100 ? (900 ? `200px` : `120px`) : `240px`}
               alt="image"
               src={`https://firebasestorage.googleapis.com/v0/b/storage-4leaf.appspot.com/o/SJPET%2Fassets%2FImages%2F%EB%A7%A4%EC%9E%A5%2F%E1%84%8E%E1%85%B5%E1%86%AF%E1%84%80%E1%85%A9%E1%86%A8%E1%84%8C%E1%85%A5%E1%86%B7.jpeg?alt=media&token=da685113-e12e-485d-b880-b4b15849ad1a`}
             />
-
             <Title>04호_칠곡점</Title>
           </ShopImage>
         </SwiperSlide>
         <SwiperSlide>
           <ShopImage>
             <Image
-              height={`240px`}
+              height={width < 1100 ? (900 ? `200px` : `120px`) : `240px`}
               alt="image"
               src={`https://firebasestorage.googleapis.com/v0/b/storage-4leaf.appspot.com/o/SJPET%2Fassets%2FImages%2F%EB%A7%A4%EC%9E%A5%2F%E1%84%91%E1%85%A9%E1%84%92%E1%85%A1%E1%86%BC%E1%84%8C%E1%85%A5%E1%86%B7.jpeg?alt=media&token=7e0f061e-96be-4b9a-bda9-13942e7dfdd1`}
             />
-
             <Title>05호_포항점</Title>
           </ShopImage>
         </SwiperSlide>
         <SwiperSlide>
           <ShopImage>
             <Image
-              height={`240px`}
+              height={width < 1100 ? (900 ? `200px` : `120px`) : `240px`}
               alt="image"
               src={`https://firebasestorage.googleapis.com/v0/b/storage-4leaf.appspot.com/o/SJPET%2Fassets%2FImages%2F%EB%A7%A4%EC%9E%A5%2F%E1%84%89%E1%85%AE%E1%84%89%E1%85%A5%E1%86%BC%E1%84%8C%E1%85%A5%E1%86%B7.jpeg?alt=media&token=dff6503d-2dc7-4958-8664-c142e73a9705`}
             />
-
             <Title>06호_수성점</Title>
           </ShopImage>
         </SwiperSlide>
         <SwiperSlide>
           <ShopImage>
             <Image
-              height={`240px`}
+              height={width < 1100 ? (900 ? `200px` : `120px`) : `240px`}
               alt="image"
               src={`https://firebasestorage.googleapis.com/v0/b/storage-4leaf.appspot.com/o/SJPET%2Fassets%2FImages%2F%EB%A7%A4%EC%9E%A5%2F%E1%84%87%E1%85%AE%E1%84%80%E1%85%A9%E1%86%A8%E1%84%8C%E1%85%A5%E1%86%B7.jpeg?alt=media&token=4f06f995-cba4-465e-83cf-4e95b8fc6fe1`}
             />
-
             <Title>07호_구포점</Title>
           </ShopImage>
         </SwiperSlide>
         <SwiperSlide>
           <ShopImage>
             <Image
-              height={`240px`}
+              height={width < 1100 ? (900 ? `200px` : `120px`) : `240px`}
               alt="image"
               src={`https://firebasestorage.googleapis.com/v0/b/storage-4leaf.appspot.com/o/SJPET%2Fassets%2FImages%2F%EB%A7%A4%EC%9E%A5%2F%E1%84%87%E1%85%AE%E1%84%80%E1%85%A9%E1%86%A8%E1%84%8C%E1%85%A5%E1%86%B7.jpeg?alt=media&token=4f06f995-cba4-465e-83cf-4e95b8fc6fe1`}
             />
-
             <Title>13호-부곡점</Title>
           </ShopImage>
         </SwiperSlide>
         <SwiperSlide>
           <ShopImage>
             <Image
-              height={`240px`}
+              height={width < 1100 ? (900 ? `200px` : `120px`) : `240px`}
               alt="image"
               src={`https://firebasestorage.googleapis.com/v0/b/storage-4leaf.appspot.com/o/SJPET%2Fassets%2FImages%2F%EB%A7%A4%EC%9E%A5%2F%E1%84%91%E1%85%A9%E1%84%92%E1%85%A1%E1%86%BC%E1%84%92%E1%85%A1%E1%86%BC%E1%84%80%E1%85%AE%E1%84%8C%E1%85%A5%E1%86%B7.jpeg?alt=media&token=a1c76b0a-0cbc-4622-b69c-bff1db6f1256`}
             />
-
             <Title>27호_포항항구점</Title>
           </ShopImage>
         </SwiperSlide>
         <SwiperSlide>
           <ShopImage>
             <Image
-              height={`240px`}
+              height={width < 1100 ? (900 ? `200px` : `120px`) : `240px`}
               alt="image"
               src={`https://firebasestorage.googleapis.com/v0/b/storage-4leaf.appspot.com/o/SJPET%2Fassets%2FImages%2F%EB%A7%A4%EC%9E%A5%2F%E1%84%89%E1%85%B5%E1%84%8C%E1%85%B5%E1%84%8C%E1%85%A5%E1%86%B7.jpeg?alt=media&token=d2a7e2b2-51a0-418f-900f-b055e25f164a`}
             />
-
             <Title>45호_시지점</Title>
           </ShopImage>
         </SwiperSlide>
         <SwiperSlide>
           <ShopImage>
             <Image
-              height={`240px`}
+              height={width < 1100 ? (900 ? `200px` : `120px`) : `240px`}
               alt="image"
               src={`https://firebasestorage.googleapis.com/v0/b/storage-4leaf.appspot.com/o/SJPET%2Fassets%2FImages%2F%EB%A7%A4%EC%9E%A5%2F%E1%84%83%E1%85%A2%E1%84%8C%E1%85%A5%E1%86%AB%E1%84%8C%E1%85%A5%E1%86%B7.jpeg?alt=media&token=fdaea8b2-898f-4496-a922-4a197a2d846f`}
             />
-
             <Title>73호_대전점</Title>
           </ShopImage>
         </SwiperSlide>
         <SwiperSlide>
           <ShopImage>
             <Image
-              height={`240px`}
+              height={width < 1100 ? (900 ? `200px` : `120px`) : `240px`}
               alt="image"
               src={`https://firebasestorage.googleapis.com/v0/b/storage-4leaf.appspot.com/o/SJPET%2Fassets%2FImages%2F%EB%A7%A4%EC%9E%A5%2F%E1%84%92%E1%85%A9%E1%86%BC%E1%84%8B%E1%85%B3%E1%86%AB%E1%84%8C%E1%85%A5%E1%86%B7.jpeg?alt=media&token=0181f68e-f55a-47dd-b241-3e6d56a9909c`}
             />
-
             <Title>87호_홍은점</Title>
           </ShopImage>
         </SwiperSlide>
