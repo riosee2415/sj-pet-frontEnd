eb63635b4463ef51d2f4ae35cb0ecbc7d22d292e
rlaehdgus27 <rlaehdgus27@gmail.com>
2021-07-08 18:10:04 +0900

    fixedNav

--- src/Components/FixedNav.jsx
+++ src/Components/FixedNav.jsx
@@ -11,6 +11,12 @@ import {
 import Theme from "../Styles/Theme";
 import { translate } from "../Components/AnimationCommon";
 import { withResizeDetector } from "react-resize-detector";
+import { CREATE_NAV_STORECONTACT } from "./FixedNavQueries";
+import useInput from "./Hooks/useInput";
+import { useMutation } from "@apollo/client";
+import { toast } from "react-toastify";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
 
 const FixedWraper = styled.div`
   position: fixed;
@@ -32,6 +38,59 @@ const FixedWraper = styled.div`
 `;
 
 const FixedNav = ({ width }) => {
+  ////////////// - USE STATE- ///////////////
+  const inputName = useInput("");
+  const inputMobile = useInput("");
+
+  ///////////// - USE MUTATION- /////////////
+  const [createNavStoreContactMutation] = useMutation(CREATE_NAV_STORECONTACT);
+
+  ///////////// - EVENT HANDLER- ////////////
+  const createNavStoreHandler = () => {
+    if (!inputName.value || inputName.value.trim() === "") {
+      toast.error("이름을 입력해주세요.");
+      return;
+    }
+    if (!inputMobile.value || inputMobile.value.trim() === "") {
+      toast.error("연락처를 입력해주세요.");
+      return;
+    }
+
+    confirmAlert({
+      title: "신청하기",
+      message: "신청하시겠습니까?",
+      buttons: [
+        {
+          label: "취소",
+          onClick: () => {
+            return false;
+          },
+        },
+        {
+          label: "확인",
+          onClick: () => createNavStoreHandlerAfter(),
+        },
+      ],
+    });
+  };
+
+  const createNavStoreHandlerAfter = async () => {
+    const { data } = await createNavStoreContactMutation({
+      variables: {
+        name: inputName.value,
+        mobile: inputMobile.value,
+      },
+    });
+
+    if (data.createNavStoreContact) {
+      toast.success("가맹점 상담을 신청하셨습니다.");
+      inputName.setValue("");
+      inputMobile.setValue("");
+    } else {
+      toast.error("잠시 후 다시 시도해주세요.");
+    }
+  };
+
   return (
     <FixedWraper>
       <Wrapper height={`100%`} dr={`row`}>
@@ -49,17 +108,17 @@ const FixedNav = ({ width }) => {
         >
           가맹문의 1588.1684
         </Wrapper>
-        <TextInput placeholder={`이름`} type={`text`} />
-        <TextInput placeholder={`연락처`} type={`number`} />
-        <Link to="/contact">
-          <CommonButton
-            width={width < 900 ? `100px` : `150px`}
-            height={width < 900 ? `30px` : `40px`}
-            fontSize={width < 900 ? `15px` : `17px`}
-          >
-            신청하기
-          </CommonButton>
-        </Link>
+        <TextInput placeholder={`이름`} type={`text`} {...inputName} />
+        <TextInput placeholder={`연락처`} type={`text`} {...inputMobile} />
+
+        <CommonButton
+          width={width < 900 ? `100px` : `150px`}
+          height={width < 900 ? `30px` : `40px`}
+          fontSize={width < 900 ? `15px` : `17px`}
+          onClick={createNavStoreHandler}
+        >
+          신청하기
+        </CommonButton>
       </Wrapper>
     </FixedWraper>
   );
